c0156c9d361e7ae59de65ba68823c93bd9959e81
mokrzycj <106016822+mokrzycj@users.noreply.github.com>
2023-05-30 00:45:02 +0200

    Przygotować migracje dla wszystkich tablic z bazy danych
    Przygotować modele dla wszystkich tablic z bazy danych
    
    Przygotować kontrolery dla wszystkich modeli
    
    Przygotować endpointy dla api

--- server/routes/api.php
+++ server/routes/api.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'v1', 'namespace' => 'app\Http\Controllers'], function () {
+    Route::apiResource('Users', UsersController::class);
+    Route::apiResource('Users_roles', UsersRolesController::class);
+    Route::apiResource('Ads', AdsController::class);
+    Route::apiResource('Ads_stats', AdsStatsControllerController::class);
+    Route::apiResource('Notifications', NotificationsController::class);
+    Route::apiResource('Invoices', InvoicesController::class);
+    Route::apiResource('Logs', LogsController::class);
+});
+
+
+Route::fallback(function () {
+    return response()->json([
+        'message' => "Ups, it seems like you tried to access a route that doesn't exist!"
+    ], 404);
+});
