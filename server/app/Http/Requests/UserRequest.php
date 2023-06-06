f0f5ae8f0876f1c529863da39e1ac9b1a463116f
mokrzycj <106016822+mokrzycj@users.noreply.github.com>
2023-06-06 23:14:41 +0200

    Dodanie Sanctum i rozbudowa UserController

--- server/app/Http/Requests/UserRequest.php
+++ server/app/Http/Requests/UserRequest.php
@@ -3,6 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
+use app\Http\Controllers\UserController;
+
 
 class UserRequest extends FormRequest
 {
@@ -11,8 +15,27 @@ class UserRequest extends FormRequest
      */
     public function authorize(): bool
     {
+        return true;
+    }
+
+    /**
+     * Check if the user is an admin.
+     *
+     * @return bool
+     */
+    protected function hasAdminPrivileges(): bool
+    {
+        if ($this->user() && $this->user()->hasAdminPrivileges()) return true;
+
         return false;
     }
+
+    protected function getMethodName(): string
+    {
+        $action = $this->route()->getActionMethod();
+        return Str::camel($action);
+    }
+
 
     /**
      * Get the validation rules that apply to the request.
@@ -21,8 +44,118 @@ class UserRequest extends FormRequest
      */
     public function rules(): array
     {
+        $methodName = $this->getMethodName();
+
+        if ($methodName === 'login') {
+            return $this->login();
+        } elseif ($methodName === 'register') {
+            return $this->register();
+        } elseif ($methodName === 'updateName') {
+            return $this->updateName();
+        } elseif ($methodName === 'updatePassword') {
+            return $this->updatePassword();
+        }
+
+        if ($this->hasAdminPrivileges() && $methodName === 'update') {
+            return $this->update();
+        }
+    }
+
+    /**
+     * Get the validation rules for login.
+     *
+     * @return array
+     */
+    protected function login(): array
+    {
         return [
-            //
+            'login' => ['required'],
+            'password' => ['required']
         ];
+    }
+
+    /**
+     * Get the validation rules for user registration.
+     *
+     * @return array
+     */
+    protected function register(): array
+    {
+        return [
+            'login' => ['required', 'unique:users'],
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required'],
+            'user_role_id' => ['required', Rule::in([1])],
+        ];
+    }
+
+    /**
+     * Get the validation rules for updating user information.
+     *
+     * @return array
+     */
+    protected function update(): array
+    {
+        return [
+            'name' => ['sometimes', 'required'],
+            'email' => ['sometimes', 'required', 'email', 'unique:users'],
+            'login' => ['sometimes', 'required', 'unique:users'],
+            'user_role_id' => ['sometimes', 'required', 'integer'],
+            'password' => ['sometimes', 'required']
+        ];
+    }
+
+    /**
+     * Get the validation rules for updating the user's name.
+     *
+     * @return array
+     */
+    protected function updateName(): array
+    {
+        return [
+            'name' => ['sometimes', 'required'],
+        ];
+    }
+
+    /**
+     * Get the validation rules for updating the user's password.
+     *
+     * @return array
+     */
+    protected function updatePassword(): array
+    {
+        return [
+            'password_current' => ['required'],
+            'password' => ['required', 'confirmed'],
+        ];
+    }
+
+    /**
+     * Prepare the data for validation.
+     */
+    protected function prepareForValidation()
+    {
+        if ($this->isMethod('POST')) {
+            $this->merge([
+                'user_role_id' => 1
+            ]);
+        } else if ($this->filled('userRoleId')) {
+            $this->merge([
+                'user_role_id' => $this->userRoleId
+            ]);
+        }
+
+        if ($this->filled('passwordConfirmation')) {
+            $this->merge([
+                'password_confirmation' => $this->passwordConfirmation
+            ]);
+        }
+
+        if ($this->filled('passwordCurrent')) {
+            $this->merge([
+                'password_current' => $this->passwordCurrent
+            ]);
+        }
     }
 }
