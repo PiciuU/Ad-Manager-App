e12f39f8f55a8f1c5edd77833720f91331d81f72
Patryk Strzałka <piciuubest@gmail.com>
2023-06-15 22:26:23 +0200

    F-37 Poprawienie headera oraz sidebara, poprawienie styli

---
+++ client/src/stores/DataStore.js
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+
+export const useDataStore = defineStore('dataStore', {
+    state: () => ({
+        sidebar: {
+            hide: false,
+            collapse: false,
+            lockScroll: false,
+        },
+    }),
+    getters: {
+        isSidebarHidden: (state) => state.sidebar.hide,
+        isSidebarCollapsed: (state) => state.sidebar.collapse,
+        isScrollDisabled: (state) => state.sidebar.lockScroll,
+    },
+    actions: {
+        collapseSidebar() {
+            this.sidebar.hide = true;
+            this.sidebar.lockScroll = false;
+            this.sidebar.collapse = !this.sidebar.collapse;
+        },
+        hideSidebar() {
+            this.sidebar.collapse = false;
+            this.sidebar.lockScroll = this.sidebar.hide;
+            this.sidebar.hide = !this.sidebar.hide;
+        }
+    }
+});
